2ef4b96e80ca5fc3588a1e07d42ff6d94693d4f0
Cyril Lepagnot <cyril@lepagnot.fr>
2018-08-09 14:56:07 +0200

    Work with retried jobs

--- lib/request_store/sidekiq/client_middleware.rb
+++ lib/request_store/sidekiq/client_middleware.rb
@@ -2,11 +2,16 @@ module RequestStore
   module Sidekiq
     class ClientMiddleware
       def call(_worker_class, item, _queue, _redis_pool = nil)
-        if ::RequestStore::Sidekiq.configuration.restore
+        if store_request_store?(item)
           item['request_store'] = ::RequestStore.store
         end
 
         yield
+      end
+
+      def store_request_store?(item)
+        ::RequestStore::Sidekiq.configuration.restore &&
+          item['request_store'].blank?
       end
     end
   end
